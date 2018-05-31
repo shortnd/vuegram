9e60dab2703648169a66cdd114f0429a6ec01b0b
shortnd <ococncol@gmail.com>
2018-05-30 22:04:07 -0400

    updated main js for page reloads

--- src/main.js
+++ src/main.js
@@ -2,12 +2,19 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import { store } from "./store.js";
+const firebase = require('./firebaseConfig.js')
 
 Vue.config.productionTip = false
 
-new Vue({
-  el: '#app',
-  router,
-  store,
-  render: h => h(App)
+// handle page reloads
+let app
+firebase.authentication.onAuthStateChanged(user => {
+  if (!app) {
+    new Vue({
+      el: '#app',
+      router,
+      store,
+      render: h => h(App)
+    })
+  }
 })
