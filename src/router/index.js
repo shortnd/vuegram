5498f14bfa1a0bddf7f529102108443570c216d7
shortnd <ococncol@gmail.com>
2018-05-30 22:15:16 -0400

    added routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,43 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import firebase from 'firebase'
+
+import Login from '@/components/Login'
+import Dashboard from '@/components/Dashboard'
+import Settings from '@/components/Settings'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
+  // This gets rid of the /#/
+  mode: 'history',
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '*',
+      redirect: '/dashboard'
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: Dashboard,
+      meta: {
+        requiredAuth: true
+      }
+    },
+    {
+      path: '/settings',
+      name: 'Settings',
+      component: Settings,
+      meta: {
+        requiredAuth: true
+      }
     }
   ]
 })
+
+export default router
